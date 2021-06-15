9d1d34036fa93b60458571d73c3d547b38879115
SG <67074147+hirvaDhandhukia@users.noreply.github.com>
2021-06-16 01:48:44 +0530

    input output processing
    a program which takes input from a user;
    on click, shows the modified output;

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-console.log(txtInput);
+// console.log(outputDiv);
+
+// // input -> when 'translate' button is clicked :
+// function clickHandler() {
+//     console.log("clicked!");
+//     console.log("input", txtInput.value)
+//     // if u want to read value then use .value with an element
+//     // if you want to write on something or put an output then use .innerText with it
+// };
+// btnTranslate.addEventListener("click", clickHandler)
+// // any function which takes another function as an input, then the inputed fn is called callback (specifically here i.e. not always);
+
 
 function clickHandler() {
-    console.log("clicked!");
-    console.log("input", txtInput.value)
+    outputDiv.innerText = "ajsjsjsjsjsjsjs   " + txtInput.value;
 };
-btnTranslate.addEventListener("click", clickHandler)
-
+btnTranslate.addEventListener("click", clickHandler);
