b93925aa00a66a086e5094e41a04117304ce9d5e
SG <67074147+hirvaDhandhukia@users.noreply.github.com>
2021-06-20 03:22:59 +0530

    update

--- app.js
+++ app.js
@@ -15,9 +15,31 @@ var outputDiv = document.querySelector("#output");
 // // any function which takes another function as an input, then the inputed fn is called callback (specifically here i.e. not always);
 // // here clickHandler is called callback function
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/pig-latin.json"
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("someting wrong with the server! try again after some time")
+}
+
 
 function clickHandler() {
-    outputDiv.innerText = "ajsjsjsjsjsjsjs   " + txtInput.value;
+    var inputText = txtInput.nodeValue;//taking input
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
 };
 btnTranslate.addEventListener("click", clickHandler);
 
